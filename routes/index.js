bea270fe8c08e6f4f9c60385c75370122def5cad
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-06-28 15:48:40 -0600

    Show uploaded images on landing page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   fs.readdir('public/images/uploads', (err, files) => {
+    files = files.filter(item => (/\.(gif|jpg|jpeg|tiff|png)$/i).test(item));
     if (err) {
       return res.render('error', { error: err });
     }
