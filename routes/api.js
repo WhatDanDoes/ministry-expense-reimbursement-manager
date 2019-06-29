e480be2755b7fdac5bfef57155cff99b5699ace4
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-06-28 19:56:23 -0600

    Prepend image type to base64 data

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const base64Img = require('base64-img');
 const router = require('express').Router();
 
 router.post('/', function(req, res, next) {
-  base64Img.img(req.body.base64Image, './public/images/uploads', timestamp.utc('YYMMDDHHmmss'), (err, filepath) => {
+  base64Img.img('data:image/jpeg;base64,'+req.body.base64Image, './public/images/uploads', timestamp.utc('YYMMDDHHmmss'), (err, filepath) => {
     if (err) {
       return res.status(500).json({ message: err.message });
     }
