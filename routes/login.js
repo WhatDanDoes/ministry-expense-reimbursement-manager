b8b61f88d6e1e6f203c607215277f5c02a8dfd84
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-05 14:27:12 -0600

    Set routing options on passport

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,12 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.post('/', passport.authenticate('local'), function(req, res) {
+const redirects = {
+  failureRedirect: '/',
+  failureFlash: 'Email or password is wrong' 
+};
+
+router.post('/', passport.authenticate('local', redirects), function(req, res) {
   req.flash('info', 'Hello, ' + req.user.email + '!');
   res.redirect('/album');
 });
