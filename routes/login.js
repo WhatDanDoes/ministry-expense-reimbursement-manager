82bf85a8c25b93c670c5776318e612dbf85d3712
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-12 19:27:20 -0600

    Include cookie as part of the JSON data

--- routes/login.js
+++ routes/login.js
@@ -24,7 +24,7 @@ router.post('/', function(req, res, next) {
         req.flash('info', 'Hello, ' + req.user.email + '!');
         return res.redirect('/');
       }
-      return res.status(201).json({message: 'Hello, ' + req.user.email + '!'});
+      return res.status(201).json({message: 'Hello, ' + req.user.email + '!', cookie: req.headers.cookie });
     });
   })(req, res, next);
 });
