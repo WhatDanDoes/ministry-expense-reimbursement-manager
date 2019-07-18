10a55aea32a2bc10c1efebb3ee4ec1387e89fee7
Daniel Bidulock <dan.bidulock@wycliffe.ca>
2019-07-18 13:31:25 -0600

    Add canRead field

--- spec/fixtures/agents.js
+++ spec/fixtures/agents.js
@@ -4,20 +4,34 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const bcrypt = require('bcrypt');
 
+/**
+ * These fixtures don't play nice with the 
+ * `unique` quality modified on arrays by
+ * `mongoose-unique-array`. These agents
+ * can't have duplicate IDs in their canRead
+ * fields for some reason.
+ */
+const danId = new ObjectId();
+const troyId = new ObjectId();
+const lannyId = new ObjectId();
+
 exports.Agent = {
   dan: {
-    _id: new ObjectId(),
+    _id: danId,
     email: 'daniel@example.com',
     password: 'secret',
+    canRead: [lannyId],
   },
   troy: {
-    _id: new ObjectId(),
+    _id: troyId,
     email: 'troy@example.com',
     password: 'topsecret',
+    canRead: [danId],
   },
   lanny: {
-    _id: new ObjectId(),
+    _id: lannyId,
     email: 'lanny@example.com',
     password: 'supersecret',
+    canRead: [troyId],
   }
 };
