9fcb6522a4526e1de0fab73ade79dbfbcd519dcb
Daniel Bidulock <daniel_bidulock@sil.org>
2021-04-21 15:47:25 -0600

    Mock modules required at runtime

--- spec/support/mockAndUnmock.js
+++ spec/support/mockAndUnmock.js
@@ -4,6 +4,8 @@
 const fs = require('fs');
 
 require('../../node_modules/negotiator/lib/mediaType');
+require('../../node_modules//iconv-lite/encodings');
+require('ejs');
 
 module.exports = function(mock) {
   return function(mocks) {
