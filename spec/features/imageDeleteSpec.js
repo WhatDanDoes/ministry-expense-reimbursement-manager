66617055522d7f1492ed2e5bfe4bc5a0d99c0eee
Daniel Bidulock <daniel_bidulock@sil.org>
2021-04-21 13:00:28 -0600

    Authenticate via Auth0

--- spec/features/imageDeleteSpec.js
+++ spec/features/imageDeleteSpec.js
@@ -1,23 +1,26 @@
 'use strict';
 
 const fixtures = require('pow-mongoose-fixtures');
-const models = require('../../models'); 
+const models = require('../../models');
 
-const app = require('../../app'); 
+const app = require('../../app');
 const request = require('supertest');
 
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 
 const Browser = require('zombie');
-const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001; 
-Browser.localhost('example.com', PORT);
+const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001;
+const DOMAIN = 'example.com';
+Browser.localhost(DOMAIN, PORT);
+
+const stubAuth0Sessions = require('../support/stubAuth0Sessions');
 
 /**
  * `mock-fs` stubs the entire file system. So if a module hasn't
- * already been `require`d the tests will fail because the 
+ * already been `require`d the tests will fail because the
  * module doesn't exist in the mocked file system. `ejs` and
- * `iconv-lite/encodings` are required here to solve that 
+ * `iconv-lite/encodings` are required here to solve that
  * problem.
  */
 const mock = require('mock-fs');
@@ -38,9 +41,9 @@ describe('DELETE /image/:domain/:agentId/:imageId', function() {
       models.Agent.findOne({ email: 'daniel@example.com' }).then(function(results) {
         agent = results;
         models.Agent.findOne({ email: 'lanny@example.com' }).then(function(results) {
-          lanny = results; 
+          lanny = results;
           models.Agent.findOne({ email: 'troy@example.com' }).then(function(results) {
-            troy = results; 
+            troy = results;
             browser.visit('/', function(err) {
               if (err) return done.fail(err);
               browser.assert.success();
@@ -81,29 +84,31 @@ describe('DELETE /image/:domain/:agentId/:imageId', function() {
 
   describe('authenticated', function() {
     beforeEach(done => {
-      mockAndUnmock({ 
-        [`uploads/${agent.getAgentDirectory()}`]: {
-          'image1.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          'image2.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          'image3.jpg': fs.readFileSync('spec/files/troll.jpg'),
-        },
-        [`uploads/${lanny.getAgentDirectory()}`]: {
-          'lanny1.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          'lanny2.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          'lanny3.jpg': fs.readFileSync('spec/files/troll.jpg'),
-        },
-        [`uploads/${troy.getAgentDirectory()}`]: {
-          'troy1.jpg': fs.readFileSync('spec/files/troll.jpg'),
-        },
-        'public/images/uploads': {}
-      });
+      stubAuth0Sessions(agent.email, DOMAIN, err => {
+        if (err) return done.fail(err);
 
-      browser.fill('email', agent.email);
-      browser.fill('password', 'secret');
-      browser.pressButton('Login', function(err) {
-        if (err) done.fail(err);
-        browser.assert.success();
-        done();
+        mockAndUnmock({
+          [`uploads/${agent.getAgentDirectory()}`]: {
+            'image1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            'image2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            'image3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+          },
+          [`uploads/${lanny.getAgentDirectory()}`]: {
+            'lanny1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            'lanny2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            'lanny3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+          },
+          [`uploads/${troy.getAgentDirectory()}`]: {
+            'troy1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+          },
+          'public/images/uploads': {}
+        });
+
+        browser.clickLink('Login', function(err) {
+          if (err) done.fail(err);
+          browser.assert.success();
+          done();
+        });
       });
     });
 
@@ -290,7 +295,7 @@ describe('DELETE /image/:domain/:agentId/:imageId', function() {
             }).catch(error => {
               done.fail(error);
             });
-          });       
+          });
 
           it('does not delete the invoice from the database', function(done) {
             models.Invoice.find({}).then((invoices) => {
